6c3831f78bcc518f2863edaec4b4195a8b18f473
Quique <quique.grases@gmail.com>
2023-04-01 21:58:45 +0200

    Fixing dragon's shader and dragon's shader editor

--- Assets/Art/Shaders/Editor/SHD_DragonEditor.cs
+++ Assets/Art/Shaders/Editor/SHD_DragonEditor.cs
@@ -1,37 +1,56 @@
 //-----------------------------------------------------------------------
-// SHD_DragonEditor.cs
+// SHD_EggEditor.cs
 //
-// Copyright 2020 Social Point SL. All rights reserved.
+// Copyright 2021 Social Point SL. All rights reserved.
 //
 //-----------------------------------------------------------------------
-using SocialPoint.TA.Utils;
 using UnityEditor;
 using UnityEngine;
 
-public class SHD_DragonEditor : SHD_EggEditor
+public class SHD_DragonEditor : BaseShaderEditor
 {
-    MaterialProperty _dissolveNoiseTexture, _dissolveEffectAmount, _dissolveColor, _dissolveEdge, _dissolveScale;
+    protected MaterialEditor MaterialEditor;
+    MaterialProperty _mainTex, _normalMap;
+    MaterialProperty _useEmission, _useMetallic;
+    MaterialProperty _emissiveMetallicMap, _emissionColor;
 
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-        base.OnGUI(materialEditor, properties);
+        this.MaterialEditor = materialEditor;
 
-        _dissolveNoiseTexture = ShaderGUI.FindProperty("_DissolveNoiseTexture", properties);
-        _dissolveScale = ShaderGUI.FindProperty("_DissolveScale", properties);
-        _dissolveEffectAmount = ShaderGUI.FindProperty("_DissolveEffectAmount", properties);
-        _dissolveColor = ShaderGUI.FindProperty("_DissolveColor", properties);
-        _dissolveEdge = ShaderGUI.FindProperty("_DissolveEdge", properties);
+        _mainTex = ShaderGUI.FindProperty("_MainTex", properties);
+        _normalMap = ShaderGUI.FindProperty("_NormalMap", properties);
+        _useEmission = ShaderGUI.FindProperty("_UseEmission", properties);
+        _useMetallic = ShaderGUI.FindProperty("_UseMetallic", properties);
+        _emissiveMetallicMap = ShaderGUI.FindProperty("_EmissiveMetallicMap", properties);
+        _emissionColor = ShaderGUI.FindProperty("_EmissionColor", properties);
 
-        Decorators.Separator();
-        Section("Dissolve Effect", SectionDissolve, false);
+        Section("Main maps", SectionMainMaps);
+        Section("Extra maps", SectionExtraMaps, true, 300);
     }
 
-    private void SectionDissolve()
+    private void SectionMainMaps()
     {
-        MaterialEditor.TexturePropertySingleLine(new GUIContent("Noise Map"), _dissolveNoiseTexture, _dissolveScale);
-        MaterialEditor.ShaderProperty(_dissolveColor, new GUIContent("Edge Color"));
-        MaterialEditor.ShaderProperty(_dissolveEdge, new GUIContent("Edge Thickness"));
+        MaterialEditor.TexturePropertySingleLine((new GUIContent(_mainTex.displayName)), _mainTex);
+        MaterialEditor.TexturePropertySingleLine(new GUIContent("Normal Map"), _normalMap);
     }
 
+    private void SectionExtraMaps()
+    {
+        MaterialEditor.ShaderProperty(_useEmission, _useEmission.displayName);
+        MaterialEditor.ShaderProperty(_useMetallic, _useMetallic.displayName);
 
+        if(_useEmission.floatValue >= 1 && _useMetallic.floatValue >= 1)
+        {
+            MaterialEditor.TexturePropertySingleLine((new GUIContent("Emissive (RGB)  Metallic (A)")), _emissiveMetallicMap, _emissionColor);
+        }
+        else if(_useEmission.floatValue >= 1 && _useMetallic.floatValue < 1)
+        {
+            MaterialEditor.TexturePropertySingleLine((new GUIContent("Emissive (RGB)")), _emissiveMetallicMap, _emissionColor);
+        }
+        else if(_useEmission.floatValue < 1 && _useMetallic.floatValue >= 1)
+        {
+            MaterialEditor.TexturePropertySingleLine((new GUIContent("Metallic (R)")), _emissiveMetallicMap);
+        }
+    }
 }
