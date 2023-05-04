d50c762808c96dfd3371bdfc1d7847a6cef200cc
Quique <quique.grases@gmail.com>
2023-05-04 22:42:32 +0200

    Updating string data file with new entrance for demo scene B

--- Assets/Scripts/StringsData.cs
+++ Assets/Scripts/StringsData.cs
@@ -3,12 +3,14 @@ namespace UOC.TFG.TechnicalDemo
     public class StringsData
     {
         public const string DEMOSCENE_A = "DemoScene_A";
-        //public const string ORBIT_CAM = "OrbitCamera";
+        public const string DEMOSCENE_B = "DemoScene_B";
         public const string ANIMATIONS = "Animations";
         public const string NEXT_ANIM = "NextAnimation";
         public const string PREV_ANIM = "PreviousAnimation";
         public const string NEXT_SKIN = "NextSkin";
         public const string PREV_SKIN = "PreviousSkin";
+        public const string LOCOMOTION = "Locomotion";
+        public const string TOMATO = "Tomato";
         public const string MENU_PAUSE = "MenuPause";
     }
 }
